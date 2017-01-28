021067991b7ab66aaa88a92873062d469ff93c7d
Masaki Murooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2017-01-28 16:19:06 +0900

    [pcl_study/RegistrationTest] print error and time. add plane algorithms.

--- pcl_study/RegistratoinTest/registration_test.cpp
+++ pcl_study/RegistratoinTest/registration_test.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctime>
 
 #include <boost/thread/thread.hpp>
 #include <Eigen/Core>
@@ -9,16 +10,25 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/filters/filter.h>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transformation_estimation_lm.h>
+#include <pcl/registration/transformation_estimation_point_to_plane.h>
+#include <pcl/registration/transformation_estimation_point_to_plane_lls.h>
 
 typedef pcl::PointXYZ PointXYZ;
-typedef pcl::PointCloud<PointXYZ> PointCloudT;
+typedef pcl::PointNormal PointN;
+typedef pcl::PointCloud<PointXYZ> PointCloudXYZ;
+typedef pcl::PointCloud<PointN> PointCloudN;
 
 
 typedef enum {
-  SVD, LM
+  SVD,
+  LM,
+  LM_Plane,
+  LLS_Plane
 } TransformationEstimationAlgorithm;
 
 
@@ -41,6 +51,10 @@ int main (int argc, char **argv)
         alg = SVD;
       } else if (optarg_string == "lm" || optarg_string == "LM") {
         alg = LM;
+      } else if (optarg_string == "lm_plane" || optarg_string == "LM_Plane" || optarg_string == "LM_PLANE") {
+        alg = LM_Plane;
+      } else if (optarg_string == "lls_plane" || optarg_string == "LLS_Plane" || optarg_string == "LLS_PLANE") {
+        alg = LLS_Plane;
       }
       break;
     case 't':
@@ -53,10 +67,11 @@ int main (int argc, char **argv)
   }
 
   // load point cloud
-  PointCloudT::Ptr object_raw (new PointCloudT);
-  PointCloudT::Ptr object (new PointCloudT);
-  PointCloudT::Ptr object_act_transformed (new PointCloudT);
-  PointCloudT::Ptr object_est_transformed (new PointCloudT);
+  PointCloudXYZ::Ptr object_raw (new PointCloudXYZ);
+  PointCloudXYZ::Ptr object (new PointCloudXYZ);
+  PointCloudXYZ::Ptr object_act_transformed (new PointCloudXYZ);
+  PointCloudXYZ::Ptr object_est_transformed (new PointCloudXYZ);
+  PointCloudN::Ptr object_act_transformed_normal (new PointCloudN);
   pcl::console::print_highlight ("Loading point clouds...\n");
   if (pcl::io::loadPCDFile<PointXYZ> (input_pcd_file, *object_raw) < 0) {
     pcl::console::print_error ("Error loading object/scene file!\n");
@@ -74,8 +89,20 @@ int main (int argc, char **argv)
   act_trans.rotate (Eigen::AngleAxisf (delta_yaw, Eigen::Vector3f::UnitZ()));
   pcl::transformPointCloud (*object, *object_act_transformed, act_trans);
 
+  // calculate norm for plane
+  if (alg == LM_Plane || alg == LLS_Plane) {
+    pcl::NormalEstimation< PointXYZ, PointN > ne;
+    ne.setInputCloud (object_act_transformed);
+    pcl::search::KdTree< PointXYZ >::Ptr tree (new pcl::search::KdTree< PointXYZ > ());
+    ne.setSearchMethod (tree);
+    ne.setRadiusSearch (0.03);
+    ne.compute (*object_act_transformed_normal);
+  }
+
   // estimate transformation
+  clock_t begin_clock = clock();
   boost::shared_ptr< pcl::registration::TransformationEstimation< PointXYZ, PointXYZ > > est;
+  boost::shared_ptr< pcl::registration::TransformationEstimation< PointXYZ, PointN > > est_normal;
   switch (alg) {
   case SVD:
     pcl::console::print_info ("Algorithm: SVD\n");
@@ -85,18 +112,52 @@ int main (int argc, char **argv)
     pcl::console::print_info ("Algorithm: LM\n");
     est.reset ( new pcl::registration::TransformationEstimationLM < PointXYZ, PointXYZ > () );
     break;
+  case LM_Plane:
+    pcl::console::print_info ("Algorithm: LM_Plane\n");
+    est_normal.reset ( new pcl::registration::TransformationEstimationPointToPlane < PointXYZ, PointN > () );
+    break;
+  case LLS_Plane:
+    pcl::console::print_info ("Algorithm: LLS_Plane\n");
+    est_normal.reset ( new pcl::registration::TransformationEstimationPointToPlaneLLS < PointXYZ, PointN > () );
+    break;
   default:
     pcl::console::print_error ("Invalid algorithm for estimating transformation!\n");
     return 1;
   }
-  est->estimateRigidTransformation(*object, *object_act_transformed, est_trans.matrix());
+  if (alg == LM_Plane || alg == LLS_Plane) {
+    est_normal->estimateRigidTransformation(*object, *object_act_transformed_normal, est_trans.matrix());
+  } else {
+    est->estimateRigidTransformation(*object, *object_act_transformed, est_trans.matrix());
+  }
+  clock_t end_clock = clock();
+  double elapsed_time = double(end_clock - begin_clock) / CLOCKS_PER_SEC;
 
   // transform pointcloud with estimated transformation
   pcl::transformPointCloud (*object, *object_est_transformed, est_trans);
 
+  // calc error
+  Eigen::Matrix3f act_rot = act_trans.matrix().topLeftCorner(3, 3);
+  Eigen::Vector3f act_rpy = act_rot.eulerAngles(0, 1, 2);
+  Eigen::Vector3f act_xyz = act_trans.matrix().block (0, 3, 3, 1);
+  Eigen::Matrix3f est_rot = est_trans.matrix().topLeftCorner(3, 3);
+  Eigen::Vector3f est_rpy = est_rot.eulerAngles(0, 1, 2);
+  Eigen::Vector3f est_xyz = est_trans.matrix().block (0, 3, 3, 1);
+  Eigen::Vector3f xyz_error = act_xyz - est_xyz;
+  double xyz_error_norm = xyz_error.norm();
+  Eigen::Matrix3f rot_error = act_rot.transpose() * est_rot;
+  Eigen::Vector3f rpy_error = rot_error.eulerAngles(0, 1, 2) ;
+  Eigen::AngleAxisf angle_axis_error;
+  double rpy_error_norm = angle_axis_error.fromRotationMatrix(rot_error).angle();
+
   // print result
+  // pcl::console::print_info ("[xyz]  actual: %f %f %f  estimated: %f %f %f\n",
+  //                           act_xyz[0], act_xyz[1], act_xyz[2], est_xyz[0], est_xyz[1], est_xyz[2]);
+  // pcl::console::print_info ("[rpy]  actual: %f %f %f  estimated: %f %f %f\n",
+  //                           act_rpy[0], act_rpy[1], act_rpy[2], est_rpy[0], est_rpy[1], est_rpy[2]);
   std::cout << "actual transformation " << std::endl << act_trans.matrix() << std::endl;
   std::cout << "estimated transformation " << std::endl << est_trans.matrix() << std::endl;
+  std::cout << "[result]  alg: " << alg << "  xyz_error: " << xyz_error_norm << " [m]  rpy_error: " << rpy_error_norm
+            << " [rad]  time: " << elapsed_time << " [sec]" << std::endl;
 
   // view point cloud
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
