e01bcdb997330fa2938b8f2ddf582eb2da92acab
mmurooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2018-02-06 15:29:21 +0900

    update comment.

--- optimization/graph_visualization_sample.py
+++ optimization/graph_visualization_sample.py
@@ -9,17 +9,18 @@ def visualize_contour(colormesh=False, cmap='jet'):
     y = np.arange(0, 10, 0.05) # y range
 
     X, Y = np.meshgrid(x, y)
-    Z = np.sin(X) + np.cos(Y)   # 表示する計算式の指定。等高線はZに対して作られる。
+    Z = np.sin(X) + np.cos(Y)   # target function
 
+    # graph
     if colormesh == True:
         plt.pcolormesh(X, Y, Z, cmap=cmap)
     else:
         plt.contour(X, Y, Z, 100, cmap=cmap)
-
-    pp=plt.colorbar(orientation="vertical") # カラーバーの表示
-    pp.set_label("Label", fontsize=24) #カラーバーのラベル
-
     plt.xlabel('X', fontsize=24)
     plt.ylabel('Y', fontsize=24)
+
+    # color bar
+    pp=plt.colorbar(orientation="vertical")
+    pp.set_label("Z", fontsize=24)
 
     plt.pause(-1)
