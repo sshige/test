0b8d09a47bf17a2cbc942c9703601cc44f324889
Masaki Murooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2019-03-22 19:20:35 +0900

    [choreonoid_ik] update sample_ik.cpp

---
+++ choreonoid_ik/README.md
@@ -0,0 +1,21 @@
+# choreonoid_ik
+
+## Build
+Need choreonoid version 1.7.
+```bash
+catkin build choreonoid_ik
+```
+
+## Execute
+
+```bash
+# FK and IK sample
+rosrun choreonoid_ik sample_ik
+```
+
+Same program with euslisp:
+```bash
+# FK and IK sample
+roscd choreonoid_ik/euslisp
+roseus sample-ik.l
+```
--- choreonoid_ik/src/sample_ik.cpp
+++ choreonoid_ik/src/sample_ik.cpp
@@ -6,6 +6,7 @@
 #include <cnoid/Link>
 #include <cnoid/EigenUtil>
 #include <cnoid/FileUtil>
+#include <ros/package.h>
 
 
 using namespace std;
@@ -14,10 +15,8 @@ using namespace Eigen;
 
 int main(int argc, char** argv)
 {
-
   // load robot model
-  // string modelfile("/home/murooka/ros/rhp_ws/src/openhrp3/sample/model/sample1.wrl");
-  string modelfile("/home/murooka/src/choreonoid/share/model/PA10/PA10.body");
+  string modelfile = getNativePathString(boost::filesystem::path(ros::package::getPath("choreonoid_ik")) / "models/PA10/PA10.body");
   BodyLoader loader;
   BodyPtr robot = loader.load(modelfile);
   if(!robot){
@@ -26,34 +25,43 @@ int main(int argc, char** argv)
   }
 
   // print robot information
+  cout << "== robot information ==" << endl;
   cout << "dof: " << robot->numJoints() << endl;
   cout << "base link name: " << robot->rootLink()->name() << endl;
   cout << "base link pos: \n" << robot->rootLink()->p() << endl;
   cout << "base link rot: \n" << robot->rootLink()->R() << endl;
 
+  // joint path information
+  cout << "== joint path information ==" << endl;
   JointPathPtr jointPathRootToEnd = getCustomJointPath(robot, robot->rootLink(), robot->link("J7"));
   Link *linkEnd = jointPathRootToEnd->endLink();
-
-  jointPathRootToEnd->joint(0)->q() = 0.5;
-  jointPathRootToEnd->joint(1)->q() = 0.5;
-  jointPathRootToEnd->joint(2)->q() = 0.5;
-  jointPathRootToEnd->joint(3)->q() = 0.5;
-  jointPathRootToEnd->joint(4)->q() = 0.5;
-  jointPathRootToEnd->joint(5)->q() = 0.5;
+  for (int i = 0; i < jointPathRootToEnd->numJoints(); i++) {
+    cout << "joint(" << i << ")->name(): " << jointPathRootToEnd->joint(i)->name() << endl;
+  }
+  cout << "linkEnd->name(): " << linkEnd->name() << endl;
 
   // calculate FK
+  cout << "== FK ==" << endl;
+  for (int i = 0; i < jointPathRootToEnd->numJoints(); i++) {
+    jointPathRootToEnd->joint(i)->q() = 0.5;
+    cout << "joint(" << i << ")->q(): " << jointPathRootToEnd->joint(i)->q() << endl;
+  }
   jointPathRootToEnd->calcForwardKinematics();
-  Vector3d currentEndPos = linkEnd->p();
-  Matrix3d currentEndRot = linkEnd->R();
-  cout << "currentEndPos: \n" << currentEndPos << endl;
-  cout << "currentEndRot: \n" << currentEndRot << endl;
+  Position currentEndCoords = linkEnd->T();
+  cout << "currentEndPos: \n" << currentEndCoords.translation() << endl;
+  cout << "currentEndRot: \n" << currentEndCoords.linear() << endl;
 
   // calculate IK
-  currentEndPos.z() -= 0.01;
-  if (jointPathRootToEnd->calcInverseKinematics(currentEndPos, currentEndRot)) {
+  cout << "== IK ==" << endl;
+ Position targetEndCoords = currentEndCoords;
+  targetEndCoords.translation().z() -= 0.01;
+  if (jointPathRootToEnd->calcInverseKinematics(targetEndCoords)) {
     for (int i = 0; i < jointPathRootToEnd->numJoints(); i++) {
       cout << "joint(" << i << ")->q(): " << jointPathRootToEnd->joint(i)->q() << endl;
     }
+    Position resultEndCoords = linkEnd->T();
+    cout << "resultEndPos: \n" << resultEndCoords.translation() << endl;
+    cout << "resultEndRot: \n" << resultEndCoords.linear() << endl;
   } else {
     cout << "failed to solve IK" << endl;
   }
